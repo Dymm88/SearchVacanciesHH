8c894567c64101cd7188cc6504055785a1f81cec
Dmitriy <ulrht88@gmail.com>
2024-04-13 20:59:49 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from params import params_SPb, params_All
 from utils import get_all_vacancies, vacancy_ids, response_vacancies
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     data = get_all_vacancies(params_SPb)
     vacancy_l = vacancy_ids(data)
     response_vacancies(vacancy_l)
--- utils.py
+++ utils.py
@@ -5,21 +5,21 @@ from message import MESSAGE_TEXT
 
 
 def get_all_vacancies(params: dict[str | int | bool]) -> list[dict]:
-    url = "https://api.hh.ru/vacancies"
+    url = 'https://api.hh.ru/vacancies'
 
     vacancy_list = []
     while True:
         r = requests.get(url=url, headers=HEADERS, params=params)
-        items = r.json()["items"]
+        items = r.json()['items']
         vacancy_list += items
-        if r.json()["pages"] == params["page"] + 1:
+        if r.json()['pages'] == params['page'] + 1:
             break
-        params["page"] += 1
+        params['page'] += 1
     return vacancy_list
 
 
 def vacancy_ids(list_data: list[dict]) -> list[int]:
-    vacancy_id = [vacancy["id"] for vacancy in list_data]
+    vacancy_id = [vacancy['id'] for vacancy in list_data]
     return vacancy_id
 
 
@@ -27,12 +27,12 @@ def response_vacancies(list_vacancies: list[int]):
     success = 0
     for item in list_vacancies:
         r = requests.post(
-            f"https://api.hh.ru/negotiations?resume_id={RESUME}&vacancy_id={str(item)}&message={MESSAGE_TEXT}",
+            f'https://api.hh.ru/negotiations?resume_id={RESUME}&vacancy_id={str(item)}&message={MESSAGE_TEXT}',
             headers=HEADERS,
         )
         if r.status_code == 201:
             success += 1
-            print(f"Резюме успешно отправлено на вакансию {item}")
+            print(f'Резюме успешно отправлено на вакансию {item}')
         else:
-            print(f"Произошла ошибка при отправке на вакансию {item}: {r.status_code}")
-    print(f"Количество отправленных отзывов - {success}")
+            print(f'Произошла ошибка при отправке на вакансию {item}: {r.status_code}')
+    print(f'Количество отправленных отзывов - {success}')
