ded70783b3bc04e40110c0de496d9f03e5f4edfc
Dmitriy <ulrht88@gmail.com>
2024-04-13 21:37:09 +0300

    refactor README.md

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(find_dotenv())
 
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-REDIRECT_URI = os.getenv('URI')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
 TOKEN = os.getenv('TOKEN')
 REFRESH_TOKEN = os.getenv('REFRESH_TOKEN')
 EMAIL = os.getenv('EMAIL')
@@ -14,6 +14,6 @@ RESUME = os.getenv('RESUME_ID')
 
 HEADERS = {
     "Authorization": f"Bearer {TOKEN}",
-    "User-Agent": f"VacancyBot/1.0 ({EMAIL})",
+    "User-Agent": f"VacancyBot/1.0 ({EMAIL})",  # VacancyBot название моего приложения, необходимо подставить свое
     "Content-Type": "application/json",
 }
